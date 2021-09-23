6650927ede3bd30ff1497e0746dea48bb8f2cfd3
chenbo90 <1205747692@qq.com>
2021-09-23 22:12:08 +0800

    feat:动态链接（指向运行区常量池的方法引用） 使用javap -v DynamicLinkTest.class查看变量指向

---
+++ jvm/src/com/cb/p55/DynamicLinkTest.java
@@ -0,0 +1,17 @@
+package com.cb.p55;
+
+public class DynamicLinkTest {
+    int num = 10;
+
+    public void methodA(){
+        System.out.println("methodA()....");
+    }
+
+    public void methodB(){
+        System.out.println("methodB()....");
+
+        methodA();
+
+        num++;
+    }
+}
